0423864dd86b0f3821c0d1cf7ac5fe7980379552
slhodges <sarahlynnsmiles@gmail.com>
2020-11-19 15:17:06 -0500

    switch overlays to java models

--- js/script.js
+++ js/script.js
@@ -1,6 +1,78 @@
-const hamburger = document.getElementById('hamburger')
-const navUL = document.getElementById('nav-ul')
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
-})
+});
+
+const open1 = document.getElementById('open1');
+const modal_container1 = document.getElementById('modal_container1');
+const close1 = document.getElementById('close1');
+
+open1.addEventListener('click', () => {
+    modal_container1.classList.add('show');
+});
+
+close1.addEventListener('click', () => {
+    modal_container1.classList.remove('show');
+});
+
+const open2 = document.getElementById('open2');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('close2');
+
+open2.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+});
+
+close2.addEventListener('click', () => {
+    modal_container2.classList.remove('show');
+});
+
+const open3 = document.getElementById('open3');
+const modal_container3 = document.getElementById('modal_container3');
+const close3 = document.getElementById('close3');
+
+open3.addEventListener('click', () => {
+    modal_container3.classList.add('show');
+});
+
+close3.addEventListener('click', () => {
+    modal_container3.classList.remove('show');
+});
+
+const open4 = document.getElementById('open4');
+const modal_container4 = document.getElementById('modal_container4');
+const close4 = document.getElementById('close4');
+
+open4.addEventListener('click', () => {
+    modal_container4.classList.add('show');
+});
+
+close4.addEventListener('click', () => {
+    modal_container4.classList.remove('show');
+});
+
+const open5 = document.getElementById('open5');
+const modal_container5 = document.getElementById('modal_container5');
+const close5 = document.getElementById('close5');
+
+open5.addEventListener('click', () => {
+    modal_container5.classList.add('show');
+});
+
+close5.addEventListener('click', () => {
+    modal_container5.classList.remove('show');
+});
+
+const open6 = document.getElementById('open6');
+const modal_container6 = document.getElementById('modal_container6');
+const close6 = document.getElementById('close6');
+
+open6.addEventListener('click', () => {
+    modal_container6.classList.add('show');
+});
+
+close6.addEventListener('click', () => {
+    modal_container6.classList.remove('show');
+});
